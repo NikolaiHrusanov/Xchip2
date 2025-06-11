70e1ed672ddee70c22dd74eb0a39cdd676acaed0
Zawak8 <viktordindev@gmail.com>
2025-06-11 15:30:45 +0300

    4

--- XStudentsChipApp/UCDownload.Designer.cs
+++ XStudentsChipApp/UCDownload.Designer.cs
@@ -30,7 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCDownload));
             this.label1 = new System.Windows.Forms.Label();
-            this.BTN_downloadLocal_UCExport = new System.Windows.Forms.Button();
+            this.BTN1_downloadLocal_UCExport = new System.Windows.Forms.Button();
             this.BTN_downloadOnline_UCExport = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -44,22 +44,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Инсталиране";
             // 
-            // BTN_downloadLocal_UCExport
+            // BTN1_downloadLocal_UCExport
             // 
-            this.BTN_downloadLocal_UCExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(164)))), ((int)(((byte)(164)))));
-            this.BTN_downloadLocal_UCExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BTN_downloadLocal_UCExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.BTN_downloadLocal_UCExport.ForeColor = System.Drawing.Color.White;
-            this.BTN_downloadLocal_UCExport.Image = ((System.Drawing.Image)(resources.GetObject("BTN_downloadLocal_UCExport.Image")));
-            this.BTN_downloadLocal_UCExport.Location = new System.Drawing.Point(105, 390);
-            this.BTN_downloadLocal_UCExport.Name = "BTN_downloadLocal_UCExport";
-            this.BTN_downloadLocal_UCExport.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.BTN_downloadLocal_UCExport.Size = new System.Drawing.Size(303, 60);
-            this.BTN_downloadLocal_UCExport.TabIndex = 11;
-            this.BTN_downloadLocal_UCExport.Text = " Локално инсталиране";
-            this.BTN_downloadLocal_UCExport.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.BTN_downloadLocal_UCExport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.BTN_downloadLocal_UCExport.UseVisualStyleBackColor = false;
+            this.BTN1_downloadLocal_UCExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(164)))), ((int)(((byte)(164)))));
+            this.BTN1_downloadLocal_UCExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN1_downloadLocal_UCExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BTN1_downloadLocal_UCExport.ForeColor = System.Drawing.Color.White;
+            this.BTN1_downloadLocal_UCExport.Image = ((System.Drawing.Image)(resources.GetObject("BTN1_downloadLocal_UCExport.Image")));
+            this.BTN1_downloadLocal_UCExport.Location = new System.Drawing.Point(105, 390);
+            this.BTN1_downloadLocal_UCExport.Name = "BTN1_downloadLocal_UCExport";
+            this.BTN1_downloadLocal_UCExport.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BTN1_downloadLocal_UCExport.Size = new System.Drawing.Size(303, 60);
+            this.BTN1_downloadLocal_UCExport.TabIndex = 11;
+            this.BTN1_downloadLocal_UCExport.Text = " Локално инсталиране";
+            this.BTN1_downloadLocal_UCExport.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BTN1_downloadLocal_UCExport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BTN1_downloadLocal_UCExport.UseVisualStyleBackColor = false;
             // 
             // BTN_downloadOnline_UCExport
             // 
@@ -83,7 +83,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.BTN_downloadOnline_UCExport);
-            this.Controls.Add(this.BTN_downloadLocal_UCExport);
+            this.Controls.Add(this.BTN1_downloadLocal_UCExport);
             this.Controls.Add(this.label1);
             this.Name = "UCDownload";
             this.Size = new System.Drawing.Size(843, 553);
@@ -95,7 +95,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button BTN_downloadLocal_UCExport;
+        private System.Windows.Forms.Button BTN1_downloadLocal_UCExport;
         private System.Windows.Forms.Button BTN_downloadOnline_UCExport;
     }
 }
